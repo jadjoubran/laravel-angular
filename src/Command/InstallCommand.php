ea1082a76e3e0fed9bf63c9418843bf94323eea6
P. R <toporny@gmail.com>
2017-09-16 23:49:51 +0100

    fire() to handle()
    because of 
    https://github.com/laravel/framework/commit/ac9f29da4785c8b942ced8c4ab308214d618a22b#diff-2aaa23817a4aeafde63888bd631364e7

--- src/Command/InstallCommand.php
+++ src/Command/InstallCommand.php
@@ -26,7 +26,7 @@ class InstallCommand extends Command
      *
      * @return void
      */
-    public function fire()
+    public function handle()
     {
         $this->installValidationErrorFormat();
 
